f1ee61bda7a54fbcff1f083d62d471cff70103f7
Christopher Conlin <cconlin@health.ucsd.edu>
2025-01-13 17:29:17 -0800

    Edited setup script

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ source ~/.tcshrc
 mkdir -p ~/Documents/MATLAB
 mv startup.m ~/Documents/MATLAB/
 
+cd ../
 git clone https://github.com/ChristopherConlin/cmig_utils.git
 git clone https://github.com/ChristopherConlin/rsi_prostate.git
 
